d7cc07ef70765d1f5fef52669159fe515a89085b
lehuy0210 <nhokhuypro78999@gmail.com>
2026-04-24 19:38:28 +0700

    Sua

--- QLBH.DAL/obj/Debug/net8.0/QLBH.DAL.AssemblyInfo.cs
+++ QLBH.DAL/obj/Debug/net8.0/QLBH.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLBH.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e49ba08bc3041c51bd0622709eae490ec5e9f211")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aea079834f2d79ad45329d7104e474121968a529")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLBH.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLBH.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
