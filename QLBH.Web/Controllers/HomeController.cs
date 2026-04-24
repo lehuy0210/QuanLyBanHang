1a1498d9a6f016c44a8ad0b6e31131c544cb1408
lehuy0210 <nhokhuypro78999@gmail.com>
2026-04-24 18:08:52 +0700

    Them LINQ loc san pham

--- QLBH.Web/Controllers/HomeController.cs
+++ QLBH.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using QLBH.DTO;
 using System.Data;
 
 namespace QLBH.Web.Controllers
@@ -14,11 +15,25 @@ namespace QLBH.Web.Controllers
             _httpClientFactory = httpClientFactory;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? CategoryID, int? SupplierID)
         {
             var client = _httpClientFactory.CreateClient();
 
-            string apiProductList = "http://localhost:5003/api/Product/List";
+            string apiProductList = "http://localhost:5003/api/Product";
+
+            var resCat = await client.GetAsync("http://localhost:5003/api/Product/categories");
+            if (resCat.IsSuccessStatusCode)
+            {
+                var jsonCat = await resCat.Content.ReadAsStringAsync();
+                ViewBag.Categories = JsonConvert.DeserializeObject<List<CategoryDTO>>(jsonCat);
+            }
+
+            var resSup = await client.GetAsync("http://localhost:5003/api/Product/suppliers");
+            if (resSup.IsSuccessStatusCode)
+            {
+                var jsonSup = await resSup.Content.ReadAsStringAsync();
+                ViewBag.Suppliers = JsonConvert.DeserializeObject<List<SupplierDTO>>(jsonSup);
+            }
 
             var response = await client.GetAsync(apiProductList);
 
@@ -28,7 +43,22 @@ namespace QLBH.Web.Controllers
 
                 DataTable dtProducts = JsonConvert.DeserializeObject<DataTable>(jsonString);
 
-                return View(dtProducts);
+                if (dtProducts != null && dtProducts.Rows.Count > 0)
+                {
+                    if (CategoryID.HasValue)
+                    {
+                        var rowsToKeep = dtProducts.AsEnumerable().Where(r => Convert.ToInt32(r["CategoryID"]) == CategoryID.Value);
+                        dtProducts = rowsToKeep.Any() ? rowsToKeep.CopyToDataTable() : dtProducts.Clone();
+                    }
+
+                    if (SupplierID.HasValue && dtProducts.Rows.Count > 0)
+                    {
+                        var rowsToKeep = dtProducts.AsEnumerable().Where(r => Convert.ToInt32(r["SupplierID"]) == SupplierID.Value);
+                        dtProducts = rowsToKeep.Any() ? rowsToKeep.CopyToDataTable() : dtProducts.Clone();
+                    }
+                }
+
+                    return View(dtProducts);
             }
             return View(new DataTable());
         }
