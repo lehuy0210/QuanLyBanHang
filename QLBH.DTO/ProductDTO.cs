f007a9bf524f28c73d2b65c78b7264991636a789
lehuy0210 <nhokhuypro78999@gmail.com>
2026-04-28 15:21:39 +0700

    Them 2 rang buoc

--- QLBH.DTO/ProductDTO.cs
+++ QLBH.DTO/ProductDTO.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization; 
+using Newtonsoft.Json;
 
 namespace QLBH.DTO
 {
     public class ProductDTO
     {
+        [JsonProperty("Id")]
+        [JsonPropertyName("Id")]
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
