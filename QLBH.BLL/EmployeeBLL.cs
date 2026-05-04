169171c2be7e77f849407c635ed4e9d32aefc7fb
lehuy0210 <nhokhuypro78999@gmail.com>
2026-05-04 13:37:40 +0700

    Sửa logic

--- QLBH.BLL/EmployeeBLL.cs
+++ QLBH.BLL/EmployeeBLL.cs
@@ -81,7 +81,7 @@ namespace QLBH.BLL
 
             for (int i = 0; i < nv.Phone.Length; i++)
             {
-                if (nv.Phone[i] < '0' && nv.Phone[i] > '9')
+                if (nv.Phone[i] < '0' || nv.Phone[i] > '9')
                 {
                     throw new Exception("Số điện thoại sai cú pháp");
                 }
